a5d606746fe3b5b8372473612a772f3944ea88a2
Marek Grzyb <b1narykill3r@gmail.com>
2018-11-01 18:56:34 +0100

    Updated Database class due to changes in Logger class

--- Database.py
+++ Database.py
@@ -7,43 +7,55 @@ from time import strftime
 from passlib.hash import pbkdf2_sha256
 
 from Config import readFromConfig
+from Framework.ErrorCodes import ERROR_FAILED_TO_INITIALIZE_DATABASE
 from Logger import Log
 from Utilities.RandomStringGenerator import GenerateRandomString
 
-logger = Log("Database", "\033[37;1m")
-logger_err = Log("Database", "\033[37;1;41m")
+logger = Log("Database")
 
 
 class Database(object):
     def __init__(self, showWelcomeMsg=False):
-        dbFileLocation = readFromConfig("database", "db_file_path")
+        dbFileLocation = readFromConfig("database", "file_path")
 
         if exists(dbFileLocation):
             if showWelcomeMsg:
-                logger.new_message('Connected to database!', 1)
+                logger.notification('Connected to database!', 2)
             self.connection = sqlite3.connect(dbFileLocation)
             self.cleanup()
         else:
-            logger.new_message('Database file not found! Initializing database...', 1)
+            logger.notification('Database file not found! Initializing database...', 1)
             self.connection = sqlite3.connect(dbFileLocation)
             try:
                 self.initializeDatabase()
-                logger.new_message('Database initialized successfully!', 1)
+                logger.notification('Database initialized successfully!', 1)
             except Exception as DBError:
-                logger_err.new_message(
-                    'There is an problem with initializing database!\nAdditional Error Info:\n' + str(DBError), 1)
-                sys.exit(6)
+                logger.error('There is an problem with initializing database!\nAdditional Error Info:\n' + str(DBError),
+                             1)
+                sys.exit(ERROR_FAILED_TO_INITIALIZE_DATABASE)
 
     def initializeDatabase(self):
-        tables = [{'Accounts': ['userID integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'EMail string UNIQUE', 'Password string', 'Birthday string', 'Country string']},
-                  {'BlockedPlayers': ['personaID integer', 'concernPersonaID integer', 'type integer', 'creationDate string']},
-                  {'Entitlements': ['userID integer', 'groupName string', 'entitlementId integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'entitlementTag string', 'version integer', 'grantDate string', 'terminationDate string', 'productId string', 'status string', 'statusReasonCode string']},
-                  {'MutedPlayers': ['personaID integer', 'concernPersonaID integer', 'type integer', 'creationDate string']},
-                  {'RecentPlayers': ['personaID integer', 'concernPersonaID integer', 'type integer', 'creationDate string']},
+        tables = [{'Accounts': ['userID integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'EMail string UNIQUE',
+                                'Password string', 'Birthday string', 'Country string']},
+                  {'BlockedPlayers': ['personaID integer', 'concernPersonaID integer', 'type integer',
+                                      'creationDate string']},
+                  {'Entitlements': ['userID integer', 'groupName string',
+                                    'entitlementId integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'entitlementTag string',
+                                    'version integer', 'grantDate string', 'terminationDate string', 'productId string',
+                                    'status string', 'statusReasonCode string']},
+                  {'MutedPlayers': ['personaID integer', 'concernPersonaID integer', 'type integer',
+                                    'creationDate string']},
+                  {'RecentPlayers': ['personaID integer', 'concernPersonaID integer', 'type integer',
+                                     'creationDate string']},
                   {'Stats': ['personaID integer', 'key string', 'value integer']},
-                  {'UsersFriends': ['personaID integer', 'concernPersonaID integer', 'type integer', 'creationDate string']},
-                  {'UsersMessages': ['senderID integer', 'receiverID integer', 'messageType string', 'messageID integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'attachments string', 'timeSent string', 'expiration integer', 'deliveryType string', 'purgeStrategy string']},
-                  {'Personas': ['personaID integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'userID integer', 'personaName string']}]
+                  {'UsersFriends': ['personaID integer', 'concernPersonaID integer', 'type integer',
+                                    'creationDate string']},
+                  {'UsersMessages': ['senderID integer', 'receiverID integer', 'messageType string',
+                                     'messageID integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'attachments string',
+                                     'timeSent string', 'expiration integer', 'deliveryType string',
+                                     'purgeStrategy string']},
+                  {'Personas': ['personaID integer PRIMARY KEY AUTOINCREMENT UNIQUE', 'userID integer',
+                                'personaName string']}]
 
         cursor = self.connection.cursor()
 
@@ -64,8 +76,10 @@ class Database(object):
         cursor.close()
 
         cursor = self.connection.cursor()
-        cursor.execute("INSERT INTO sqlite_sequence (name, seq) VALUES (?,?)", ("Accounts", 3,))  # Start counting Accounts from 4 (3 are already occupied by server(s))
-        cursor.execute("INSERT INTO sqlite_sequence (name, seq) VALUES (?,?)", ("Personas", 3,))  # Start counting Personas from 4 (3 are already occupied by server(s))
+        cursor.execute("INSERT INTO sqlite_sequence (name, seq) VALUES (?,?)",
+                       ("Accounts", 3,))  # Start counting Accounts from 4 (3 are already occupied by server(s))
+        cursor.execute("INSERT INTO sqlite_sequence (name, seq) VALUES (?,?)",
+                       ("Personas", 3,))  # Start counting Personas from 4 (3 are already occupied by server(s))
         self.connection.commit()
         cursor.close()
 
@@ -100,10 +114,10 @@ class Database(object):
 
             self.addDefaultEntitlements(self.loginUser(email, password)['UserID'])
 
-            logger.new_message('Successfully registered new account (' + email + ')!', 1)
+            logger.notification('Successfully registered new account (' + email + ')!', 1)
             return True
         except:
-            logger_err.new_message('User with this email (' + email + ') are currently registered!', 1)
+            logger.warning('User with this email (' + email + ') are currently registered!', 1)
             return False
 
     def checkIfEmailTaken(self, email):
@@ -130,10 +144,6 @@ class Database(object):
         else:
             return False
 
-    def registerSession(self):
-        session = GenerateRandomString(27) + "."
-        return session
-
     def loginUser(self, email, password):
         cursor = self.connection.cursor()
         cursor.execute("SELECT * FROM Accounts WHERE EMail = ?", (email,))
@@ -142,7 +152,7 @@ class Database(object):
 
         if data is not None:
             if pbkdf2_sha256.verify(password, data[2]):
-                session = self.registerSession()
+                session = GenerateRandomString(27) + "."
                 return {'UserID': data[0], 'SessionID': session}
             else:
                 return {'UserID': 0}  # Provided password is incorrect
@@ -174,7 +184,7 @@ class Database(object):
             return None
         else:
             personaId = data[0]
-            session = self.registerSession()
+            session = GenerateRandomString(27) + "."
 
             return {'lkey': session, 'personaId': personaId}
 
@@ -229,25 +239,35 @@ class Database(object):
 
         defaultEntitlements = [(forUserID, 'NoVetRank', "BFBC2NAM:PC:NOVETRANK", 0, currentTime, "", "", "ACTIVE", "",),
                                (forUserID, '', "ONLINE_ACCESS", 0, currentTime, "", "DR:156691300", "ACTIVE", "",),
-                               (forUserID, 'AddsVetRank', "BFBC2:PC:ADDSVETRANK", 0, currentTime, "", "", "ACTIVE", "",),  # sometimes clients don't have this in the packet...whatever
-                               (forUserID, 'BFBC2PC', 'BETA_ONLINE_ACCESS', 0, currentTime, "", "OFB-BFBC:19121", "ACTIVE", "",)]  # beta access is nice too (though it doesnt seem to affect anything)
+                               (
+                               forUserID, 'AddsVetRank', "BFBC2:PC:ADDSVETRANK", 0, currentTime, "", "", "ACTIVE", "",),
+                               # sometimes clients don't have this in the packet...whatever
+                               (forUserID, 'BFBC2PC', 'BETA_ONLINE_ACCESS', 0, currentTime, "", "OFB-BFBC:19121",
+                                "ACTIVE", "",)]  # beta access is nice too (though it doesnt seem to affect anything)
 
-        if readFromConfig("emulator", "new_players_have_vietnam"):
-            defaultEntitlements.append((forUserID, 'BFBC2PC', "BFBC2:PC:VIETNAM_ACCESS", 0, currentTime, "", "DR:219316800", "ACTIVE", ""))
-            defaultEntitlements.append((forUserID, 'BFBC2PC', "BFBC2:PC:VIETNAM_PDLC", 0, currentTime, "", "DR:219316800", "ACTIVE", ""))
+        if readFromConfig("players", "new_players_have_vietnam"):
+            defaultEntitlements.append(
+                (forUserID, 'BFBC2PC', "BFBC2:PC:VIETNAM_ACCESS", 0, currentTime, "", "DR:219316800", "ACTIVE", ""))
+            defaultEntitlements.append(
+                (forUserID, 'BFBC2PC', "BFBC2:PC:VIETNAM_PDLC", 0, currentTime, "", "DR:219316800", "ACTIVE", ""))
 
-        if readFromConfig("emulator", "new_players_have_premium"):
-            defaultEntitlements.append((forUserID, 'BFBC2PC', "BFBC2:PC:LimitedEdition", 1, currentTime, "", "OFB-BFBC:19120", "ACTIVE", ""))
+        if readFromConfig("players", "new_players_have_premium"):
+            defaultEntitlements.append(
+                (forUserID, 'BFBC2PC', "BFBC2:PC:LimitedEdition", 1, currentTime, "", "OFB-BFBC:19120", "ACTIVE", ""))
 
-        if readFromConfig("emulator", "new_players_have_specact"):
-            defaultEntitlements.append((forUserID, 'BFBC2PC', "BFBC2:PC:ALLKIT", 0, currentTime, "", "DR:192365600", "ACTIVE", ""))
+        if readFromConfig("players", "new_players_have_specact"):
+            defaultEntitlements.append(
+                (forUserID, 'BFBC2PC', "BFBC2:PC:ALLKIT", 0, currentTime, "", "DR:192365600", "ACTIVE", ""))
 
-        if readFromConfig("emulator", "new_players_are_veterans"):
-            defaultEntitlements.append((forUserID, 'AddsVetRank', "BF3:PC:ADDSVETRANK", 0, currentTime, "", "OFB-EAST:40873", "ACTIVE", ""))
+        if readFromConfig("players", "new_players_are_veterans"):
+            defaultEntitlements.append(
+                (forUserID, 'AddsVetRank', "BF3:PC:ADDSVETRANK", 0, currentTime, "", "OFB-EAST:40873", "ACTIVE", ""))
 
         for entitlement in defaultEntitlements:
             cursor = self.connection.cursor()
-            cursor.execute("INSERT INTO Entitlements (userID, groupName, entitlementTag, version, grantDate, terminationDate, productId, status, statusReasonCode) VALUES (?,?,?,?,?,?,?,?,?)", entitlement)
+            cursor.execute(
+                "INSERT INTO Entitlements (userID, groupName, entitlementTag, version, grantDate, terminationDate, productId, status, statusReasonCode) VALUES (?,?,?,?,?,?,?,?,?)",
+                entitlement)
 
             self.connection.commit()
             cursor.close()
@@ -275,7 +295,8 @@ class Database(object):
         currentTime = strftime('%Y-%m-%dT%H:%MZ')
 
         cursor = self.connection.cursor()
-        cursor.execute("INSERT INTO " + type + " (personaID, concernPersonaID, type, creationDate) VALUES (?,?,?,?)", (ownerID, toPersonaID, ownerType, currentTime))
+        cursor.execute("INSERT INTO " + type + " (personaID, concernPersonaID, type, creationDate) VALUES (?,?,?,?)",
+                       (ownerID, toPersonaID, ownerType, currentTime))
 
         self.connection.commit()
         cursor.close()
@@ -303,13 +324,17 @@ class Database(object):
 
         for user in toUsers:
             cursor = self.connection.cursor()
-            cursor.execute("INSERT INTO UsersMessages (senderID, receiverID, messageType, attachments, timeSent, expiration, deliveryType, purgeStrategy) VALUES (?,?,?,?,?,?,?,?)", (fromPersona, user, str(messageType), str(attachments), str(currentTime), expires, str(deliveryType), str(purgeStrategy),))
+            cursor.execute(
+                "INSERT INTO UsersMessages (senderID, receiverID, messageType, attachments, timeSent, expiration, deliveryType, purgeStrategy) VALUES (?,?,?,?,?,?,?,?)",
+                (fromPersona, user, str(messageType), str(attachments), str(currentTime), expires, str(deliveryType),
+                 str(purgeStrategy),))
 
             self.connection.commit()
             cursor.close()
 
             cursor = self.connection.cursor()
-            cursor.execute("SELECT messageID FROM UsersMessages WHERE senderID = ? AND receiverID = ? AND timeSent = ?", (fromPersona, toUsers[0], currentTime,))
+            cursor.execute("SELECT messageID FROM UsersMessages WHERE senderID = ? AND receiverID = ? AND timeSent = ?",
+                           (fromPersona, toUsers[0], currentTime,))
             data = cursor.fetchone()
 
             if data is not None:
@@ -339,7 +364,7 @@ class Database(object):
                                      'deliveryType': str(message[7]),
                                      'purgeStrategy': str(message[8])})
                 else:
-                    logger_err.new_message("WARNING: Found incorrect message! Removing it...", 2)
+                    logger.warning("WARNING: Found incorrect message! Removing it...", 2)
                     self.deleteMessages([message[3]])
 
         return messages
