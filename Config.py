804be0845327f3e54b332bf6583e4bb9da0a7b13
Marek Grzyb <b1narykill3r@gmail.com>
2018-11-01 18:49:23 +0100

    Changed way we store config

--- Config.py
+++ Config.py
@@ -1,22 +1,20 @@
+from colorama import Fore
+
 import os
+import commentjson
 
-from ConfigParser import ConfigParser
+from Framework.ErrorCodes import ERROR_FAILED_TO_LOAD_CONFIG, ERROR_FAILED_TO_READ_CONFIG
 
-configFile = ConfigParser()
-configFile.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.ini'))
+try:
+    configFile = commentjson.load(file(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config.json')))
+except:
+    print Fore.RED + "Failed to load config.json\nThis program cannot continue" + Fore.RESET
+    os._exit(ERROR_FAILED_TO_LOAD_CONFIG)
 
 
 def readFromConfig(section, parameter):
-    value = configFile.get(section, parameter)
-
-    isString = isinstance(value, str)
-
-    if isString:
-        if value.lower() == "true":
-            return True
-        elif value.lower() == "false":
-            return False
-        else:
-            return value
-    else:
-        return value
+    try:
+        return configFile[section][parameter]
+    except:
+        print Fore.RED + "Failed to load certain values in the config.json, be sure that EVERY option has a valid value and try it again.\nThis program cannot continue" + Fore.RESET
+        os._exit(ERROR_FAILED_TO_READ_CONFIG)
