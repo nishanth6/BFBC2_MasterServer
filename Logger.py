5716ee3a2352220f5a3e06ea7a201f2af3f4808a
Marek Grzyb <b1narykill3r@gmail.com>
2018-11-01 18:50:30 +0100

    Rewrited Logger

--- Logger.py
+++ Logger.py
@@ -1,4 +1,4 @@
-from colorama import init
+from colorama import init, Fore
 
 from time import gmtime, strftime
 from os import remove
@@ -6,46 +6,78 @@ from os import remove
 from Config import readFromConfig
 
 
+def start_log():
+    global log_filename
+
+    if not readFromConfig("debug", "log_timestamp"):
+        log_filename = "server.log"
+
+        try:
+            remove(log_filename)
+        except:
+            pass
+    else:
+        log_filename = "server_" + str(strftime("%Y_%m_%d__%H_%M_%S", gmtime())) + ".log"
+
+
 class Log(object):
-    def __init__(self, messageFrom, messageColor):
+
+    notificationColor = "\33[97m"
+    warningColor = Fore.YELLOW
+    errorColor = Fore.RED
+
+    plasmaClientNotification = Fore.CYAN
+    plasmaServerNotification = Fore.BLUE
+    theaterClientNotification = Fore.GREEN
+    theaterServerNotification = Fore.MAGENTA
+    httpNotification = "\33[96m"
+
+    def __init__(self, messageFrom=""):
         init()
 
         self.messageFrom = messageFrom
-        self.messageColor = messageColor
 
         self.logFile = readFromConfig("debug", "create_log")
-        self.logTimestamp = readFromConfig("debug", "put_timestamp_in_log")
+        self.logTimestamp = readFromConfig("debug", "log_timestamp")
         self.fileLogLevel = int(readFromConfig("debug", "file_log_level"))
         self.consoleLogLevel = int(readFromConfig("debug", "console_log_level"))
 
         self.useColors = readFromConfig("console", "use_colors")
 
-    def new_message(self, message, level=0):
-        timestamp = str(strftime("%H:%M:%S", gmtime()))
+    def notification(self, message, level=0):
+        notificationColors = {
+            "PlasmaClient": Log.plasmaClientNotification,
+            "PlasmaServer": Log.plasmaServerNotification,
+            "TheaterClient": Log.theaterClientNotification,
+            "TheaterServer": Log.theaterServerNotification,
+            "WebServer": Log.httpNotification
+        }
+
+        self.__new_message(message, notificationColors.get(self.messageFrom, Log.notificationColor), level)
+
+    def warning(self, message, level=0):
+        self.__new_message(message, Log.warningColor, level)
+
+    def error(self, message, level=0):
+        self.__new_message(message, Log.errorColor, level)
+
+    def __new_message(self, message, color, level):
+        timestamp = str(strftime("%d.%m.%Y %H:%M:%S", gmtime()))
 
         if self.logFile:
             if level <= self.fileLogLevel:
-                saveToLog = ""
-                if self.logTimestamp:
-                    saveToLog += "[" + timestamp + "]"
+                saveToLog = "[" + timestamp + "][" + self.messageFrom + "] " + message + "\n"
 
-                saveToLog += "[" + self.messageFrom + "]"
-
-                saveToLog += " " + message + "\n"
-
-                with open("server.log", "a") as logfile:
-                    logfile.write(saveToLog)
+                try:
+                    with open(log_filename, "a") as logfile:
+                        logfile.write(saveToLog)
+                except NameError:
+                    start_log()
 
         if level <= self.consoleLogLevel:
             consoleMessage = "[" + timestamp + "]" + "[" + self.messageFrom + "] " + message
 
             if self.useColors:
-                consoleMessage = self.messageColor + consoleMessage + "\33[0m"
+                consoleMessage = color + consoleMessage + Fore.RESET
 
             print(consoleMessage)
-
-    def clean_log(self):
-        try:
-            remove("server.log")
-        except:
-            pass
